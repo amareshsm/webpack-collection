afbdf487b9ebe7ae0315d38c70c43ab9b7b47284
amareshsm <30730124+amareshsm@users.noreply.github.com>
2021-04-01 23:59:56 +0530

    webpack-react

---
+++ webpack-react/src/index.js
@@ -0,0 +1,9 @@
+const sampleObject = {
+    data:['hi','hello'],
+    optional:false
+}
+let a = () =>{
+    let newObj={...sampleObject,newContent:'hi'}
+    console.log(newObj,sampleObject);
+}
+a();
